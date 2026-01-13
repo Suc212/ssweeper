11b1707a003561ca61434219c0c37f854f12cd20
omotayo olukale <omotayo.olukale@gmail.com>
2026-01-13 10:25:44 +0100

    added clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,13 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 fbq('init', '745704095240857');
 fbq('track', 'PageView');`}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v0qk4egz47");`}
+        </Script>
       </head>
       <body className={`font-sans antialiased`}>
         {children}
